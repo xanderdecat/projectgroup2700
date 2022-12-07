4b4543934611f36635618c09f406c034c289f05a
xanderdecat <xander@decat.be>
2022-12-07 14:20:37 +0100

    update

--- src/main/java/com/example/projectgroup27/AddUserController.java
+++ src/main/java/com/example/projectgroup27/AddUserController.java
@@ -1,4 +1,54 @@
 package com.example.projectgroup27;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Date;
+
 public class AddUserController {
+
+    @FXML
+    private Button RegisterButton;
+
+    @FXML
+    private DatePicker dateOfBirth;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private TextField firstName;
+
+    @FXML
+    private TextField lastName;
+
+    @FXML
+    private TextField phoneNumber;
+
+    public void goToHomePage(ActionEvent actionEvent) {
+
+        try {
+            String firstName1 = firstName.getText();
+            String lastName1 = lastName.getText();
+            Date dob = dateOfBirth.get
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("HomeScreenPage.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 630, 400);
+            Stage stage = new Stage();
+            stage.setTitle("Home screen");
+            stage.setScene(scene);
+            stage.show();
+            ((Node)(actionEvent.getSource())).getScene().getWindow().hide();
+        }
+        catch (IOException e) {
+        }
+    }
 }
