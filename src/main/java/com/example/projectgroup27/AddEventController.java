678fb442fd58ebf3cacf63e13a79e930152fcc8e
xanderdecat <xander@decat.be>
2022-12-07 13:54:49 +0100

    update

---
+++ src/main/java/com/example/projectgroup27/AddEventController.java
@@ -0,0 +1,9 @@
+package com.example.projectgroup27;
+
+import javafx.event.ActionEvent;
+
+public class AddEventController {
+    public void makeNewEvent(ActionEvent actionEvent) {
+
+    }
+}
--- src/main/java/com/example/projectgroup27/HomeScreenPageController.java
+++ src/main/java/com/example/projectgroup27/HomeScreenPageController.java
@@ -1,4 +1,14 @@
 package com.example.projectgroup27;
 
+import javafx.event.ActionEvent;
+
 public class HomeScreenPageController {
+    public void logOut(ActionEvent actionEvent) {
+    }
+
+    public void goToCreateNewEvent(ActionEvent actionEvent) {
+    }
+
+    public void goToSeePreviousEvents(ActionEvent actionEvent) {
+    }
 }
